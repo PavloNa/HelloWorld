27b1bf083ad8c04a21837446ddf7624f62068ec2
Pavlo <pavlo.escola@gmail.com>
2024-05-26 00:26:47 +0100

    cover added

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This website is my online portfolio.
 
+https://www.freepik.com/free-ai-image/door-leading-magical-world_94543134.htm#fromView=search&page=1&position=26&uuid=8194ebad-d45c-4d56-ace1-f5b815953605
--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { useRef, useEffect, useState } from 'react';
 
 import {gsap, Power3} from 'gsap'
+import coverimg from './images/intro.png';
 
 function App() {
   let app = useRef(null)
   let text = useRef(null)
+  let cover = useRef(null)
 
   const [textSize, setTextSize] = useState(false);
 
@@ -22,18 +24,26 @@ function App() {
   useEffect(() => {
     gsap.to(app, 0, {css: {visibility: 'visible'}})
     gsap.to(text, .8, {opacity: 1, y: -20, ease: Power3.easeOut})
+    gsap.to(cover, 0, {css: {visibility: 'visible'}})
   }, [])
 
   return (
+    <>
     <div className="App" ref={el => app = el}>
       <header className="App-header">
+      </header>
+      <body className="App-body">
+        <div>
+          <img className='intro-pic' src={coverimg} loading='eager' ref={el => {cover = el}}></img>
+        </div>
         <div className='Main-text' ref={el => {text = el}} onClick={textSize !== true ? handleExpand : handleShrink}> 
         <p>
           My portfolio
         </p>
         </div>
-      </header>
+      </body>
     </div>
+    </>
   );
 }
 
