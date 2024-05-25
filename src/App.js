eb51e641411bf1901c7bee8918371f636c239090
Pavlo <pavlo.escola@gmail.com>
2024-05-25 21:56:33 +0100

    Simple annimation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useEffect, useState } from 'react';
+
+import {gsap, Power3} from 'gsap'
 
 function App() {
+  let app = useRef(null)
+  let text = useRef(null)
+
+  const [textSize, setTextSize] = useState(false);
+
+  const handleExpand = () => {
+    gsap.to(text, .8, {fontSize: 'calc(20px + 2vmin)', ease: Power3.easeOut})
+    setTextSize(true)
+  }
+
+  const handleShrink = () => {
+    gsap.to(text, .8, {fontSize: 'calc(10px + 2vmin)', ease: Power3.easeOut})
+    setTextSize(false)
+  }
+
+  useEffect(() => {
+    gsap.to(app, 0, {css: {visibility: 'visible'}})
+    gsap.to(text, .8, {opacity: 1, y: -20, ease: Power3.easeOut})
+  }, [])
+
   return (
-    <div className="App">
+    <div className="App" ref={el => app = el}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className='Main-text' ref={el => {text = el}} onClick={textSize !== true ? handleExpand : handleShrink}> 
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My portfolio
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </div>
       </header>
     </div>
   );
